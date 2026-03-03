1f273835cec55f3d429c42dd65803ebbeb4e2410
Daiyan Abrar Samin <daiyan03samin@gmail.com>
2026-03-03 13:20:47 -0500

    New Fire Store project code implemented

--- Demos/firestoredbdemo/lib/main.dart
+++ Demos/firestoredbdemo/lib/main.dart
@@ -1,125 +1,148 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Call the Firebase to initialize the app
+  // because we want API to integrate to the platform
+  // for seamless integration
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+        apiKey: "AIzaSyBRNZbByufV0wwdMWTqVYilCwahwp8p7lc",
+        appId: "450511959923",
+        messagingSenderId: "1:450511959923:android:1ef7e04900ad994b3b7706",
+        projectId: "firestoresample-55b56"),
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: FSDemoSample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class FSDemoSample extends StatefulWidget {
+  const FSDemoSample({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<FSDemoSample> createState() => _FSDemoSampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FSDemoSampleState extends State<FSDemoSample> {
+  // In Fire Store, the files call Documents and a list of documents call Collection
+  // This line will create a space in the cloud with a reference name Users
+  CollectionReference users = FirebaseFirestore.instance.collection('Users');
+  // String username = '';
+  // String password = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  // Add the data to the cloud
+  Future<void> addUser() async {
+    String username = usernameController.text;
+    String password = passwordController.text;
+
+    if (username.isNotEmpty && password.isNotEmpty) {
+      await users.add({'username': username, 'password': password});
+      setState(() {
+        username = '';
+        password = '';
+      });
+
+      clearFields();
+    }
+  }
+
+  // Update an entity(doc)
+  Future<void> updateUser(String id) async {
+    await users.doc(id).update({'username': 'new name', 'password': 'vanier'});
+    clearFields();
+  }
+
+  // Delete an entity(doc)
+  Future<void> deleteUser(String id) async {
+    await users.doc(id).delete();
+    clearFields();
+  }
+
+  void clearFields() {
+    usernameController.clear();
+    passwordController.clear();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("FS Demo"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        children: [
+          TextField(
+            controller: usernameController,
+            // onChanged: (value) => username = value,
+            decoration: InputDecoration(hintText: "Enter Username"),
+          ),
+          SizedBox(height: 10,),
+          TextField(
+            controller: passwordController,
+            // onChanged: (value) => password = value,
+            decoration: InputDecoration(hintText: "Enter Password"),
+          ),
+          SizedBox(height: 15,),
+          ElevatedButton(
+              onPressed: addUser,
+              child: Text("Add to FS")
+          ),
+          SizedBox(height: 10,),
+          // Expanded defies animation conflict between ListView and Column
+          Expanded( // StreamBuilder: date is pulled in real-time
+              child: StreamBuilder<QuerySnapshot>(
+                // Snapshot is th DB chunk for data manipulation
+                stream: users.snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData)
+                    return Text("Loading ...");
+                  return ListView(
+                    // Write a map function to convert to Fire Store data to Dart object
+                    // Snapshot data is a complete docs and map to each doc to Dart object
+                      children: snapshot.data!.docs.map((doc) {
+                        return ListTile(
+                          title: Text(doc['username']),
+                          subtitle: Text('Password: ${doc['password']}'),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                  onPressed: () => updateUser(doc.id),
+                                  icon: Icon(Icons.edit)
+                              ),
+                              IconButton(
+                                  onPressed: () => deleteUser(doc.id),
+                                  icon: Icon(Icons.delete)
+                              ),
+                            ],
+                          ),
+                        );
+                      }).toList()
+                  );
+                },
+              )
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
